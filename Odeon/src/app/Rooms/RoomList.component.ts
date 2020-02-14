7f5419c25cb5cbb5e89de97594096d861fc1c252
berke125 <ersayanberke@gmail.com>
2020-02-14 17:32:47 +0300

    odeon hotel

--- Odeon/src/app/Rooms/RoomList.component.ts
+++ Odeon/src/app/Rooms/RoomList.component.ts
@@ -9,20 +9,31 @@ import { UserService } from '../shared/USER.service';
 export class RoomListComponent implements OnInit
 {
   hotel_users: any=[];
-  public myModel: string = '1';
+  public myModel: string = '';
   //public odano: string = '';
   //public no: number;
   public isim: string = '';
-  
+  public sayac: number = 0;
   constructor(private service: UserService, private router: Router) { }
   AllRooms()
   {
- //this.service.GetRoom(this.odano).subscribe((data:any) => this.hotel_users = data);
-    this.service.GetRoom().subscribe((data) => this.hotel_users = data);
+    //this.service.GetRoom(this.odano).subscribe((data:any) => this.hotel_users = data);
+    this.service.GetRoom().subscribe((data) => { this.hotel_users = data; this.Bos_Oda_Saydir() });
   }
   EmptyRooms()
   {
-    this.service.GetRooms(this.myModel,this.isim).subscribe((data) => this.hotel_users = data);
+    this.service.GetRooms(this.myModel, this.isim).subscribe((data) => this.hotel_users = data);
+
+  }
+  Bos_Oda_Saydir()
+  {
+    this.sayac = 0;
+    for (let i = 0; i < this.hotel_users.length; i++) {
+
+      if (this.hotel_users[i].No == 0) {
+        this.sayac++;
+      }
+    }
   }
   ngOnInit()
   {
